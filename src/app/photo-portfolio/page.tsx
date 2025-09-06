111603f994eea79b4a5a3527e25c08f61d8c26bd
wlg223 <wesguarneri@gmail.com>
2025-09-06 15:24:36 -0400

    adding src files

---
+++ src/app/photo-portfolio/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useRef, useLayoutEffect } from "react"
+
+
+export default function PhotoPortfolioPage() {
+
+
+  return (
+    <main className="min-h-screen bg-black text-white px-6 py-20">
+      Portfolio Page
+    </main>
+  )
+}
